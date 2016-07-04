34dad4a4e0d47d4ca23591923b7ace933acb0779
Daniel Brown <csengineer13@gmail.com>
2016-07-03 23:26:56 -0400

    Adding models to help convey errors

---
+++ lug.io.ViewModel/Common/SimpleError.cs
@@ -0,0 +1,8 @@
+﻿namespace lug.io.ViewModel.Common
+{
+    public class SimpleError
+    {
+        public string Key { get; set; }
+        public string ErrorMessage { get; set; }
+    }
+}
---
+++ lug.io.ViewModel/Common/SimpleModelState.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections.Generic;
+
+namespace lug.io.ViewModel.Common
+{
+    public class SimpleModelState
+    {
+
+        public bool IsValid { get; set; }
+        public List<SimpleError> PropertyErrors { get; set; }
+        public List<SimpleError> ModelErrors { get; set; }
+
+        public void AddError(string property, string message)
+        {
+            if (string.IsNullOrWhiteSpace(property))
+            {
+                ModelErrors.Add(new SimpleError { Key = property, ErrorMessage = message });
+            }
+            else
+            {
+                PropertyErrors.Add(new SimpleError { Key = property, ErrorMessage = message });
+            }
+        }
+    }
+}
